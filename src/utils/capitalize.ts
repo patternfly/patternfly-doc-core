932ece8b3962344a34aeb1e1e2b1f018155d5535
Donald Labaj <donald.labaj@gmail.com>
2024-11-21 23:42:50 -0500

    fix module error.

--- src/utils/capitalize.ts
+++ src/utils/capitalize.ts
@@ -1,7 +1,3 @@
-function capitalize(input: string): string {
+export function capitalize(input: string): string {
   return input[0].toUpperCase() + input.substring(1);
 }
-
-module.exports = {
-  capitalize
-};
