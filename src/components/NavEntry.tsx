37ed3cf153236c8ee70c6df1246d0718484af4a8
Austin Sullivan <ausulliv@redhat.com>
2024-12-10 16:23:08 -0500

    feat(structure): Add groundwork for routing and navigation of content

---
+++ src/components/NavEntry.tsx
@@ -0,0 +1,26 @@
+import { NavItem } from '@patternfly/react-core'
+
+export interface TextContentEntry {
+  id: string
+  data: {
+    id: string
+    section: string
+  }
+  collection: string
+}
+
+interface NavEntryProps {
+  entry: TextContentEntry
+  isActive: boolean
+}
+
+export const NavEntry = ({ entry, isActive }: NavEntryProps) => {
+  const { id } = entry
+  const { id: entryTitle, section } = entry.data
+
+  return (
+    <NavItem itemId={id} to={`/${section}/${id}`} isActive={isActive}>
+      {entryTitle}
+    </NavItem>
+  )
+}
---
+++ src/components/NavSection.tsx
@@ -0,0 +1,37 @@
+import { NavExpandable } from '@patternfly/react-core'
+import { sentenceCase } from 'change-case'
+import { NavEntry, type TextContentEntry } from './NavEntry'
+
+interface NavSectionProps {
+  entries: TextContentEntry[]
+  sectionId: string
+  activeItem: string
+}
+
+export const NavSection = ({
+  entries,
+  sectionId,
+  activeItem,
+}: NavSectionProps) => {
+  const isExpanded = window.location.pathname.includes(sectionId)
+
+  const sortedNavEntries = entries.sort((a, b) =>
+    a.data.id.localeCompare(b.data.id),
+  )
+
+  const isActive = sortedNavEntries.some((entry) => entry.id === activeItem)
+
+  const items = sortedNavEntries.map((entry) => (
+    <NavEntry key={entry.id} entry={entry} isActive={activeItem === entry.id} />
+  ))
+
+  return (
+    <NavExpandable
+      title={sentenceCase(sectionId)}
+      isActive={isActive}
+      isExpanded={isExpanded}
+    >
+      {items}
+    </NavExpandable>
+  )
+}
