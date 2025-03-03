682486feafe05fd1cec71728ee40cde7d9ecc696
Austin Sullivan <ausulliv@redhat.com>
2025-03-03 10:44:48 -0500

    chore(CI): rename release.config.mjs to release.config.js

---
+++ release.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  branches: ['main'],
+  analyzeCommits: {
+    preset: 'angular',
+  },
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    '@semantic-release/github',
+  ],
+  dryRun: false,
+}
