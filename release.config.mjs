7f3cc0218be96a4a85d5921e95f22d6a30da55d6
Austin Sullivan <ausulliv@redhat.com>
2025-03-03 11:22:05 -0500

    Revert "chore(CI): remove semantic-release config file"
    This reverts commit b94205d854d95518e53b151b0ad76a6e4f312c40.

---
+++ release.config.mjs
@@ -0,0 +1,16 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+export default {
+  branches: ['main'],
+  analyzeCommits: {
+    preset: 'angular',
+  },
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    '@semantic-release/github',
+  ],
+  dryRun: false,
+}
