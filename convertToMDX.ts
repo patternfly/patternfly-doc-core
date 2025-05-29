57d8cd5d06c01f6f8b572a5e8754ff133fe65076
Austin Sullivan <ausulliv@redhat.com>
2025-05-29 14:16:21 -0400

    DO NOT MERGE: complete test conversion script for react

--- convertToMDX.ts
+++ convertToMDX.ts
@@ -10,18 +10,22 @@ async function main() {
 
     //regex link: https://regexr.com/8f0er
     const importRegex = /(?<!```no[lL]ive\n)import {?[\w\s,\n]*}?.*\n/g
+
+    //removes all top level imports from the md file that the old docs framework used to determine what imports are needed
     const withoutImports = fileContent.replace(importRegex, '')
 
     //regex link: https://regexr.com/8f0bu
-    const ExampleBlockRegex = /```[tj]s file=['"]\.\/(\w*)\.(\w*)['"]\s*\n```/g
+    const ExampleBlockRegex = /```[tj]s file=['"]\.?\/?(\w*)\.(\w*)['"]\s*\n```/g
 
     //the first capture group is the example file name without the extension or path, the second is the extension
+    const replacementString = `\nimport $1 from "./$1.$2?raw"\n\n<LiveExample src={$1} />`
     const examplesConvertedToMDX = withoutImports.replace(
       ExampleBlockRegex,
-      `\nimport $1 from "./$1.$2?raw"\n\n<LiveExample src={$1} />`,
+      replacementString,
     )
 
-    const noLiveRemoved = examplesConvertedToMDX.replace(/```no[lL]ive/g, "```")
+    //we want to strip the nolive/noLive tags from codeblocks as that was custom to the old docs framework
+    const noLiveRemoved = examplesConvertedToMDX.replace(/```no[lL]ive/g, '```')
 
     await writeFile(file + 'x', noLiveRemoved)
   })
--- src/components/LiveExample.tsx
+++ src/components/LiveExample.tsx
@@ -1,8 +1,10 @@
-import React, { useState, Fragment, useRef } from 'react'
+import React, { useState, Fragment, useRef, useEffect, createRef, useReducer } from 'react'
 import { convertToReactComponent } from '@patternfly/ast-helpers'
-import { ErrorBoundary } from "react-error-boundary"
+import { ErrorBoundary } from 'react-error-boundary'
 import * as reactCoreModule from '@patternfly/react-core'
-import * as reactIconsModule from "@patternfly/react-icons"
+import * as reactIconsModule from '@patternfly/react-icons'
+import styles from "@patternfly/react-styles/css/components/_index"
+import * as reactTokensModule from "@patternfly/react-tokens"
 import { ExampleToolbar } from './ExampleToolbar'
 interface LiveExampleProps {
   src: string
@@ -12,14 +14,19 @@ function fallbackRender({ error }: any) {
   return (
     <div role="alert">
       <p>Something went wrong:</p>
-      <pre style={{ color: "red" }}>{error.message}</pre>
+      <pre style={{ color: 'red' }}>{error.message}</pre>
     </div>
-  );
+  )
 }
 
-
 function getLivePreview(editorCode: string) {
-  const scope = { ...reactCoreModule, ...reactIconsModule, ...{ useState, Fragment, useRef } }
+  const scope = {
+    ...reactCoreModule,
+    ...reactIconsModule,
+    styles,
+    ...reactTokensModule,
+    ...{ useState, Fragment, useRef, useEffect, createRef, useReducer },
+  }
   const { code: transformedCode } = convertToReactComponent(editorCode)
 
   const componentNames = Object.keys(scope)
