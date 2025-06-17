22c353b2e700863e9cd5c1a3bc9ad6594eda57da
Donald Labaj <dlabaj@gmail.com>
2025-06-17 12:13:24 -0400

    feat: Updated the cli to support deploying to cloud flare.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 
-import node from '@astrojs/node';
-
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
